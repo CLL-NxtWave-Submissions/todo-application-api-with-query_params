c59b5383f86df4fc738d727668582edc7c9d96e8
Surya T N <suryatej.nim@yahoo.com>
2022-08-24 11:56:46 +0000

    Implemented server-side handler for client requests to API end-point: GET /todos, with query parameters to filter requested todo items

--- app.js
+++ app.js
@@ -30,6 +30,68 @@ const initializeDBAndServer = async () => {
 
 initializeDBAndServer();
 
+const validTodoItemPriorityValues = ["HIGH", "MEDIUM", "LOW"];
+const validTodoItemStatusValues = ["TO DO", "IN PROGRESS", "DONE"];
+const validTodoItemCategoryValues = ["WORK", "HOME", "LEARNING"];
+
+/*
+    Function Name        : isValidValue
+    Input Parameters     : 
+        - validValues    : Array of valid values
+        - inputValue     : Input query parameter
+                           value.
+    Return Value         : Boolean (true/false)
+    ---------------------------------------------
+    Description : Function expression to check if
+                  input value in a query parameter
+                  matches an entry in the array/set
+                  of valid values.
+*/
+const isValidValue = (validValues, inputValue) => {
+  return validValues.includes(inputValue);
+};
+
+/*
+    Function Name        : checkQueryParametersValidity
+    Input Parameters     : 
+        - validValues    : Array of valid values
+        - inputValue     : Input query parameter
+                           value.
+    Return Value         : Boolean (true/false)
+    ----------------------------------------------------
+    Description : Function to check validity of
+                  input query parameter values:
+                  priority, status, category and
+                  return info on first instance
+                  of invalid input value.
+*/
+const checkQueryParametersValidity = (queryParameterValues) => {
+  let validityCheckResult = {
+    isSuccess: true,
+    checkFailedMessage: "",
+  };
+
+  const { priority, status, category } = queryParameterValues;
+  if (priority !== "" && !isValidValue(validTodoItemPriorityValues, priority)) {
+    validityCheckResult.isSuccess = false;
+    validityCheckResult.checkFailedMessage = "Invalid Todo Priority";
+  } else if (
+    status !== "" &&
+    !isValidValue(validTodoItemStatusValues, status)
+  ) {
+    validityCheckResult.isSuccess = false;
+    validityCheckResult.checkFailedMessage = "Invalid Todo Status";
+  } else if (
+    category !== "" &&
+    !isValidValue(validTodoItemCategoryValues, category)
+  ) {
+    validityCheckResult.isSuccess = false;
+    validityCheckResult.checkFailedMessage = "Invalid Todo Category";
+  }
+
+  return validityCheckResult;
+};
+
 /*
     End-Point 1      : GET /todos
     Query Parameters : search_q, priority,
@@ -38,5 +100,44 @@ initializeDBAndServer();
     To fetch todo items based on the
     search criteria in the query parameters.
 */
+app.get("/todos", async (req, res) => {
+  const {
+    search_q = "",
+    priority = "",
+    status = "",
+    category = "",
+  } = req.query;
 
+  const queryParametersValidityCheckResult = checkQueryParametersValidity({
+    priority,
+    status,
+    category,
+  });
+
+  if (!queryParametersValidityCheckResult.isSuccess) {
+    res.status(400);
+    res.send(queryParametersValidityCheckResult.checkFailedMessage);
+  } else {
+    // Valid query parameter input
+    const queryToFilterAndFetchTodoData = `
+        SELECT
+            *
+        FROM
+            todo
+        WHERE
+            todo LIKE '%${search_q}%'
+        AND
+            priority LIKE '%${priority}%'
+        AND 
+            status LIKE '%${status}%'
+        AND
+            category LIKE '%${category}%';
+        `;
+
+    const filteredTodoItemData = await todoAppDBConnectionObj.all(
+      queryToFilterAndFetchTodoData
+    );
+    res.send(filteredTodoItemData);
+  }
+});
 module.exports = app;
